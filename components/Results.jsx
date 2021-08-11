1f5697c8674b0f56e24f024e94d6fa4caf8ab178
dilip stack <designsdilip@gmail.com>
2021-08-11 17:54:49 +0545

    production bug fixed

--- components/Results.jsx
+++ components/Results.jsx
@@ -5,7 +5,7 @@ import FlipMove from "react-flip-move"
 function Results({ results }) {
     return (
         <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap">
-            {results.map(result => {
+            {results?.map(result => {
                 return(
                 <Thumbnail key={result.id} result={result}/>
                 )
